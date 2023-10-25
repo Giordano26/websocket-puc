49fb77ca826e44ac4d6b4bbf4dd1411da3f37de3
Stefano Giordano <stef@MacBook-Air-de-Stefano.local>
2023-10-25 10:31:07 -0300

    feat: server start with threading

--- websocket/server.py
+++ websocket/server.py
@@ -19,3 +19,26 @@ server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #anything that hits the address -> addr(HOST + PORT), will be on this socket
 server.bind(ADDR)
 
+def start():
+    server.listen()
+    while True:
+        #starts listening and waits a conn and addr from client
+        #conn will allow to communicate back to the client 
+        #addr -> info about ip and port is connect to the server
+        conn, addr = server.accept() 
+
+        #create a thread for handle client requisitions
+        thread = threading.Thread(target=client_handle, args= (conn,addr))
+        thread.start()
+
+        #shows actual connections
+        print(f"[NEW CONECCTION] New connection from {conn}/{addr}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+def client_handle(conn, addr):
+    pass
+
+
+
+print(f"[STARTING] Server is starting on {HOST} at port {PORT}...")
+start()
