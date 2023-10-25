43f62731ca381b2bdfbae73fd2be50f8d20ef21c
Stefano Giordano <stef@MacBook-Air-de-Stefano.local>
2023-10-25 09:40:14 -0300

    refact: code refact

--- examples/message.py
+++ examples/message.py
@@ -1,11 +1,11 @@
 import asyncio
 import websockets
 
-SERVER_IP = "ws://localhost:"
+HOST = "ws://localhost:"
 PORT = 8765
 
 async def msg():
-    uri = SERVER_IP + str(PORT)
+    uri = HOST + str(PORT)
     async with websockets.connect(uri) as websocket:
         await websocket.send("Eu sou o Jonga")
         msg_send = await websocket.recv()
