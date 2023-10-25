f6a5affd6e99ba816523bd6e340aad135a77c67f
Stéfano Giordano <stefano.giordano@ubisoft.com>
2023-10-25 16:54:12 -0300

    feat: server side done, client handle ok

--- websocket/server.py
+++ websocket/server.py
@@ -14,6 +14,13 @@ ADDR = (HOST, PORT)
 #header(first 64 bytes) from requisition -> will tell the size of the msg for sockek recv
 HEADER = 64
 
+#defining the format of char is utf-8 enconding 
+FORMAT = 'utf-8'
+
+
+#flag for client disconnection
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
 #Create a new socket
 #socket.AF_INET -> socket family IPV4
 #socket.SOCK_STREAM -> streaming data on the socket
@@ -35,17 +42,28 @@ def start():
         thread.start()
 
         #shows actual connections
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        print(f"[ACTIVE CONNECTIONS]: {threading.active_count() - 1}")
 
 def client_handle(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    print(f"[NEW CONNECTION]: {addr} connected.")
 
     connected = True
 
     while connected:
-        msg = conn.recv()
+        #parsing the header to integer to grab the size of char stream from client message
+        msg_lenght = int(conn.recv(HEADER).decode(FORMAT))
+        
+        #actual message from client before parsing the header
+        msg = conn.recv(msg_lenght).decode(FORMAT)
+
+        if msg != DISCONNECT_MESSAGE:
+            print(f"[{addr}]: {msg}")
+        else:
+            print(f"[DISCONNECTING]: client {addr} is disconnecting...")
+            connected = False
+
+    conn.close()
 
 
-
-print(f"[STARTING] Server is starting on {HOST} at port {PORT}...")
+print(f"[STARTING]: Server is starting on {HOST} at port {PORT}...")
 start()
